805e1f798e2134f3ae64d3c6257eacac1341eb2c
Denis Reshetnyak <denresh81@yandex.ru>
2022-07-24 16:57:17 +0700

    mesto]

--- backend/helpers/jwt.js
+++ backend/helpers/jwt.js
@@ -1,6 +1,8 @@
 const jwt = require('jsonwebtoken');
 
+const { NODE_ENV, JWT_SECRET } = process.env;
+
 const SECRET_KEY = 'secret_key';
 
 module.exports.generateToken = (payload) => jwt.sign({ _id: payload }, SECRET_KEY, { expiresIn: '7d' });
-module.exports.checkToken = (token) => jwt.verify(token, SECRET_KEY);
+module.exports.checkToken = (token) => jwt.verify(token, NODE_ENV ? JWT_SECRET : 'secret');

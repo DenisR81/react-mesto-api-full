ad004d62b79ede6cb40b7c149c00643d8cdf9ba1
Denis Reshetnyak <denresh81@yandex.ru>
2022-07-24 18:51:36 +0700

    mesto

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -2,13 +2,10 @@ const ErrorUnauthorized = require('../errors/ErrorUnauthorized');
 const { checkToken } = require('../helpers/jwt');
 
 module.exports.isAuth = (req, res, next) => {
-  const auth = req.cookies.jwt;
-
-  if (!auth || !auth.startsWith('Bearer ')) {
-    return next(new ErrorUnauthorized('Необходима авторизация'));
+  const token = req.cookies.jwt;
+  if (!token) {
+    throw new ErrorUnauthorized('ОНеобходима авторизация');
   }
-
-  const token = auth.replace('Bearer ', '');
   let payload;
 
   try {

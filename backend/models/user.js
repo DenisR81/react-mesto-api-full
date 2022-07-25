eb62e94f7d3dfe7302e544486d4f98955977a19e
Denis Reshetnyak <denresh81@yandex.ru>
2022-07-25 14:55:44 +0700

    mesto

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -41,5 +42,19 @@ const userSchema = new mongoose.Schema({
     minlength: 6,
   },
 });
-
+userSchema.statics.findUserByCredentials = function findUserByCredentials(email, password) {
+  return this.findOne({ email }).select('+password')
+    .then((user) => {
+      if (!user) {
+        return Promise.reject(new Error('Неправильные почта или пароль'));
+      }
+      return bcrypt.compare(password, user.password)
+        .then((matched) => {
+          if (!matched) {
+            return Promise.reject(new Error('Неправильные почта или пароль'));
+          }
+          return user;
+        });
+    });
+};
 module.exports = mongoose.model('user', userSchema);

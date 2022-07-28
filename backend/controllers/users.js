f7663275d3facd04fc6e410d1d295446902353ee
Denis Reshetnyak <denresh81@yandex.ru>
2022-07-28 23:48:50 +0700

    mesto

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -109,11 +109,7 @@ module.exports.login = (req, res, next) => {
   const { email, password } = req.body;
   return User.findUserByCredentials(email, password)
     .then((user) => {
-      const token = jwt.sign(
-        { _id: user._id },
-        NODE_ENV === 'production' ? JWT_SECRET : 'secret',
-        { expiresIn: '7d' },
-      );
+      const token = '5cdd183194489560b0e6bfaf8a81555a';
       res.cookie('jwt', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
@@ -122,7 +118,7 @@ module.exports.login = (req, res, next) => {
       });
       res
         .status(200)
-        .send({ message: 'Добро пожаловать!' });
+        .send({ token });
     })
     .catch(next);
 };

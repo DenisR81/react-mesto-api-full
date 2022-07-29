c47e65f1004206272293925179374ad7582e69e9
Denis Reshetnyak <denresh81@yandex.ru>
2022-07-30 02:09:54 +0700

    mesto

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -120,7 +120,9 @@ module.exports.login = (req, res, next) => {
         sameSite: 'none',
         secure: true,
       });
-      res.status(200).send(NODE_ENV === 'production' ? JWT_SECRET : 'secret', {token});
+      res
+        .status(200)
+        .send({ token });
     })
     .catch(next);
 };
